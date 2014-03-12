ca0ebdb73afa2098900d7ea13236e47cebaecc42
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2014-03-12 13:21:39 -0300

    [WIP] infecting with tests

--- opina.js
+++ opina.js
@@ -1,92 +1,44 @@
 function Phrases() {
-  var PhrasesInit = function() {
-    this.phrases = this.collection = [
-      {
-        preffix: "+1",
-        suffix: "!",
-        concat: false,
-      },
-      {
-        preffix: "",
-        suffix: "? E se de vez disso, a gente for estar fazendo outra coisa?",
-        concat: true,
-      },
-      {
-        preffix: "Também!",
-        suffix: "",
-        concat: false,
-      },
-      {
-        preffix: "",
-        suffix: "? Ah vá ráríró caváááálo.",
-        concat: true,
-      },
-      {
-        preffix: "",
-        suffix: "? Não sei o que é isso.",
-        concat: true,
-      }
-    ];
+  function PhrasesInit() {
+    this.all = []
 
-    var randomPhrase = new Chooser(this)
-
-    this.phraseFor = function(subject) {
-      var phrase = randomPhrase.next()
-      var newPhrase = phrase.preffix
-      if(phrase.concat)
-        newPhrase = newPhrase + subject
-      return newPhrase + phrase.suffix
+    this.add = function(options) {
+      var phrase = new Phrases.Phrase(options)
+      this.all.push(phrase)
     }
   }
 
   return new PhrasesInit();
 }
 
-Phrases.Phrase = function() {
+Phrases.Phrase = function(options) {
   return {
-    prefix: prefix,
-    suffix: suffix,
-    concat: concat
-  }
-}
-
-function Chooser(collectionHolder) {
-  var collection = collectionHolder.collection
-  return {
-    next: function() {
-      var random = Math.floor(Math.random() * collection.length);
-      return collection[random]
+    preffix: options.preffix || "",
+    suffix: options.suffix || "",
+    concat: options.concat || false,
+    say: function(what) {
+      var newPhrase = this.preffix
+      if(this.concat) {
+        newPhrase += " " + what
+      }
+      return newPhrase +" "+ this.suffix
     }
   }
 }
 
-function Background(collection) {
-  var BgInit = function() {
-    this.backgrounds = this.collection = collection
-  }
-  return new BgInit();
-}
-
-function Opinator($scope) {
-  var background = new Background([
-      "bg0.jpg", "bg1.jpg",
-      "bg2.jpg", "bg3.jpg",
-    ])
-  var phrases = new Phrases()
-  var bgChooser = new Chooser(background)
-
-  $scope.randomBackground = bgChooser.next()
-
-  $scope.canShowCurrentOpinion = function() {
-    return $scope.currentOpinion &&
-      $scope.currentOpinion.length > 0
-  }
-
-  $scope.newSubject = function() {
-    if($scope.subject.length == 0) {
-      $scope.currentOpinion = ""
-      return
+function Chooser(list) {
+  var defaultRandomizer = {
+    random: function(upTo) {
+      return Math.floor(Math.random() * upTo)
     }
-    $scope.currentOpinion = phrases.phraseFor($scope.subject)
+  }
+
+  return {
+    collection: list.collection,
+    next: function(randomizer) {
+      var randomizer = randomizer || defaultRandomizer
+      var idx = randomizer.random(this.collection.length)
+      return this.collection[idx]
+    }
   }
 }
--- tests/opina_tests.js
+++ tests/opina_tests.js
@@ -7,3 +7,64 @@ describe("Phrases", function() {
     })
   })
 })
+
+describe("Phrases.Phrase", function() {
+  it("uses constructor parameter as source of property values", function() {
+    var phrase = Phrases.Phrase({preffix: "oi"})
+    expect(phrase.preffix).toEqual("oi")
+  })
+
+  it("just ignores invalid properties", function() {
+    var phrase = Phrases.Phrase({sua: "mae"})
+    expect(phrase.sua).toBeUndefined()
+  })
+
+  describe("say", function() {
+    var phrase = new Phrases.Phrase({
+      preffix: "Holla.",
+      suffix: "Yo soy el Zuruki!",
+      concat: true,
+    })
+
+    it("uses preffix and suffix to form the new phrase", function() {
+      var finalPhrase = "Holla. Linduxos. Yo soy el Zuruki!"
+      expect(phrase.say("Linduxos.")).toEqual(finalPhrase)
+    })
+
+    it("ignores the subject when concat is `false`", function() {
+      phrase.concat = false
+      var finalPhrase = "Holla. Yo soy el Zuruki!"
+      expect(phrase.say("Linduxos.")).toEqual(finalPhrase)
+    })
+  })
+})
+
+describe("Chooser", function() {
+  var list = {collection: ["1", "2", "3"]}
+  var chooser = new Chooser(list)
+  var randomizer = {
+    _random: "",
+    upTo: null,
+    random: function(upTo) {
+      this.upTo = upTo
+      return this._random
+    },
+    setRandom: function(random) { this._random = random },
+  }
+
+  beforeEach(function() {
+    randomizer.upTo = null
+  })
+
+  describe("next", function(){
+    it("uses the `randomizer.random` to retrieve the next random", function() {
+      randomizer.setRandom(0)
+      expect(chooser.next(randomizer)).toEqual("1")
+    })
+
+    it("passes `collection.length` to the `randomizer.random`", function() {
+      chooser.next(randomizer)
+      expect(randomizer.upTo).toEqual(list.collection.length)
+    })
+  })
+})
